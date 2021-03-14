5280f00b281e02ec593d25b67754019c6449c512
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2021-03-14 20:51:26 +0100

    change add-project-to-smmr.sh to make it parametric

--- labs/observe-jaeger/add-project-to-smmr.sh
+++ labs/observe-jaeger/add-project-to-smmr.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-oc patch servicemeshmemberroll/default \
--n istio-system --type=merge \
--p '{"spec": {"members": ["tracing"]}}'
+display_usage() {
+    echo -e "\nUsage: $0 project \n"
+}
+
+if [ $# -le 0 ]; then
+    display_usage
+    exit 1
+fi
+
+if [[ ($# == "--help") || $# == "-h" ]]; then
+    display_usage
+    exit 0
+fi
+
+#oc patch servicemeshmemberroll/default -n istio-system --type=merge -p '{"spec": {"members": ["tracing"]}}'
+oc -n istio-system patch --type='json' smmr default -p "[{\"op\": \"add\", \"path\": \"/spec/members/-\", \"value\":\"$1\"}]"
+
