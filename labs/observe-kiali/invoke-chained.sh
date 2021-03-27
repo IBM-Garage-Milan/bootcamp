d29f9f7feb927a889ccc6683a77fc3155c6b71fe
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2021-03-27 09:44:38 +0100

    move btm

--- labs/observe-kiali/invoke-chained.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GATEWAY_URL=$(oc get route istio-ingressgateway -n istio-system -o template --template '{{ "http://" }}{{ .spec.host }}')
-
-while true;
- do curl ${GATEWAY_URL}/chained;
- sleep 3;
-done
