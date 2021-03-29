adf7f69ba1aaa6e13dfe3760c67b7014f01b5758
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2021-03-29 13:08:59 +0200

    prepare lab matrics

--- Service-Mesh/labs/observe-kiali/invoke-greet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GATEWAY_URL=$(oc get route istio-ingressgateway -n istio-system -o template --template '{{ "http://" }}{{ .spec.host }}')
-
-while true;
- do curl ${GATEWAY_URL}/greet;
- sleep 3;
-done
--- Service-Mesh/labs/resilience-timeout/response-times.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-GATEWAY_URL=`oc get route istio-ingressgateway -n istio-system -o template --template '{{ "http://" }}{{ .spec.host }}'`
-
-curl  -s \
-  -w 'HTTP code: %{http_code}\nTime: %{time_total}s\n' \
-  "${GATEWAY_URL}/resilience-timeout"
