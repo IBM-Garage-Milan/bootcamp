e6190321cb679f2401f27238f9edf282e7b2637c
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2021-03-29 13:17:17 +0200

    prepate lab tekton

--- Service-Mesh/labs/secure-authc/patch-smmr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env sh
-
-oc patch smmr default -n istio-system --type='json' -p='[{"op": "add", "path": "/spec/members/0", "value":"curl"}]'
--- Service-Mesh/solutions/secure-mesh/add-labels.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-oc -n secure-mesh patch deployment history --type='json' -p='[{"op": "add", "path": "/spec/template/metadata/labels/ns-restricted", "value":"true"}]'
-
-oc -n secure-mesh patch deployment currency --type='json' -p='[{"op": "add", "path": "/spec/template/metadata/labels/ns-restricted", "value":"true"}]'
