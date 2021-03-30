492e97474a9f405683f7494108458724d81a5b22
MASSIMO CAPRINALI <massimo.caprinali@it.ibm.com>
2021-03-30 19:55:40 +0200

    add lab-traffic

---
+++ labs/istio-yaml/README.md
@@ -0,0 +1,5 @@
+# ISTIO class yaml source
+
+Change history:
+
+- 09/12/2018 Initial creation - for istio 1.0.0 exercises 
--- labs/observe-metrics/get-prometheus-url.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-oc get route prometheus -n istio-system \
--o template --template '{{ "https://" }}{{ .spec.host }}'
